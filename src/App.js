10a4d9b8bbe321bbac603262bb5d2ee78e05ebe4
RudiDonnot <149707597+RudiDonnot@users.noreply.github.com>
2024-09-27 03:35:55 +0200

    bug ?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
-import MasonryGrid from "./pages/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<MasonryGrid />} />
+        <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
     </Router>
